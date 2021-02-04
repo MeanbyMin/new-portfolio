bcaa3f3ea0405791f0b4b2a371b11953f1573507
MeanbyMin <soeg0810@gmail.com>
2021-02-04 17:41:50 +0900

    dothome에 db생성 후 주소 맞춰주기

--- login/include/dbconn.php
+++ login/include/dbconn.php
@@ -1,3 +1,4 @@
 <?php
-    $conn = mysqli_connect("127.0.0.1", "root", "dnjsqls92", "meanbymin") or die("데이터 베이스 연결 실패 !");
+    header('Content-Type: text/html; charset=UTF-8');
+    $conn = mysqli_connect("localhost", "soeg0810", "dnjsqls92!", "soeg0810") or die("데이터 베이스 연결 실패 !");  //   닷홈이 가지고 있는 mysql에 DB를 만들어서 실행시킴.
 ?>
