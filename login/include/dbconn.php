50506996e259dc51f7d6dbea7bf320d7823d421b
MeanbyMin <soeg0810@gmail.com>
2021-02-06 03:59:54 +0900

    db연결

--- login/include/dbconn.php
+++ login/include/dbconn.php
@@ -1,4 +1,3 @@
 <?php
-    header('Content-Type: text/html; charset=UTF-8');
-    $conn = mysqli_connect("localhost", "soeg0810", "dnjsqls92!", "soeg0810") or die("데이터 베이스 연결 실패 !");  //   닷홈이 가지고 있는 mysql에 DB를 만들어서 실행시킴.
+    $conn = mysqli_connect("localhost", "soeg0810", "dnjsqls92!", "soeg0810") or die("데이터 베이스 연결 실패 !");
 ?>
