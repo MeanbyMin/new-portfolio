98de941e6524f02fc03ed2e2b5c5ce8dcca8ea5c
MeanbyMin <soeg0810@gmail.com>
2021-02-10 21:52:37 +0900

    회원가입 페이지

--- login/signup.php
+++ login/signup.php
@@ -10,62 +10,62 @@
     <link rel="stylesheet" href="./css/signup.css" type="text/css">
     <script src="./js/signup.js"></script>
     <script src="https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
-<script>
-    function sample6_execDaumPostcode() {
-        new daum.Postcode({
-            oncomplete: function(data) {
-                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+    <script>
+        function sample6_execDaumPostcode() {
+            new daum.Postcode({
+                oncomplete: function(data) {
+                    // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
-                // 각 주소의 노출 규칙에 따라 주소를 조합한다.
-                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-                var addr = ''; // 주소 변수
-                var extraAddr = ''; // 참고항목 변수
+                    // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+                    // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                    var addr = ''; // 주소 변수
+                    var extraAddr = ''; // 참고항목 변수
 
-                //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-                if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
-                    addr = data.roadAddress;
-                } else { // 사용자가 지번 주소를 선택했을 경우(J)
-                    addr = data.jibunAddress;
+                    //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+                    if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                        addr = data.roadAddress;
+                    } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                        addr = data.jibunAddress;
+                    }
+
+                    // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+                    if(data.userSelectedType === 'R'){
+                        // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                        // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                        if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                            extraAddr += data.bname;
+                        }
+                        // 건물명이 있고, 공동주택일 경우 추가한다.
+                        if(data.buildingName !== '' && data.apartment === 'Y'){
+                            extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                        }
+                        // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                        if(extraAddr !== ''){
+                            extraAddr = ' (' + extraAddr + ')';
+                        }
+                        // 조합된 참고항목을 해당 필드에 넣는다.
+                        document.getElementById("sample6_extraAddress").value = extraAddr;
+                    
+                    } else {
+                        document.getElementById("sample6_extraAddress").value = '';
+                    }
+
+                    // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                    document.getElementById('sample6_postcode').value = data.zonecode;
+                    document.getElementById("sample6_address").value = addr;
+                    // 커서를 상세주소 필드로 이동한다.
+                    document.getElementById("sample6_detailAddress").focus();
                 }
-
-                // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
-                if(data.userSelectedType === 'R'){
-                    // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-                    // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-                    if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-                        extraAddr += data.bname;
-                    }
-                    // 건물명이 있고, 공동주택일 경우 추가한다.
-                    if(data.buildingName !== '' && data.apartment === 'Y'){
-                        extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-                    }
-                    // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-                    if(extraAddr !== ''){
-                        extraAddr = ' (' + extraAddr + ')';
-                    }
-                    // 조합된 참고항목을 해당 필드에 넣는다.
-                    document.getElementById("sample6_extraAddress").value = extraAddr;
-                
-                } else {
-                    document.getElementById("sample6_extraAddress").value = '';
-                }
-
-                // 우편번호와 주소 정보를 해당 필드에 넣는다.
-                document.getElementById('sample6_postcode').value = data.zonecode;
-                document.getElementById("sample6_address").value = addr;
-                // 커서를 상세주소 필드로 이동한다.
-                document.getElementById("sample6_detailAddress").focus();
-            }
-        }).open();
-    }
-</script>
+            }).open();
+        }
+    </script>
 </head>
 <body>
     <div id="wrap">
         <!--Header-->
         <div id="header" role="logo">
             <h1>
-                <a href="#"><img src="./img/logo.png" alt="MeanByMin">
+                <a href="./login.php"><img src="./img/logo.png" alt="MeanByMin">
                 </a>
             </h1>
         </div>
@@ -77,11 +77,17 @@
                         <h3 class="join_title">
                             <label for="id">아이디</label>
                         </h3>
-                        <span class="box int_id">
-                            <input type="text" name="userid" id="userid" title="ID" class="int" maxlength="20">
-                            <span class="id_url">@meanbymin.com</span>
+                        <span class="box id_box">
+                            <span class="int_id">
+                                <input type="hidden" name="isIdCheck" id="isIdCheck" value="false">
+                                <input type="text" name="userid" id="userid" title="ID" class="" maxlength="20" onkeyup="isIdChange()">
+                                <span class="id_url">@meanbymin.com</span>
+                            </span>
+                            <input type="button" value="중복 확인" class="btn_type btn_number" onclick="idCheck()">
                         </span>
+                        <p id="idCheck_text"></p>
                         <p class="sub int_id_sub">아이디를 4글자 이상 20글자 이하로 입력하세요.</p>
+                        <p class="sub int_id_sub2">아이디 중복체크를 확인하세요.</p>
                     </div>
                     <div class="join_row">
                         <h3 class="join_title">
@@ -156,7 +162,7 @@
                         <div class="box int_ssn_area">
                             <span class="int_ssn">
                                 <input type="hidden" name="isSSN" id="isSSN" value="false">
-                                <input type="text" name="ssn1" id="ssn1" class="int" maxlength="6" onkeyup="moveFocus()"> - <input type="text" name="ssn2" id="ssn2" class="int" maxlength="7">
+                                <input type="text" name="ssn1" id="ssn1" class="int" maxlength="6" value="001011" readonly onkeyup="moveFocus()"> - <input type="text" name="ssn2" id="ssn2" class="int" maxlength="7" value="3068518" readonly>
                             </span>
                             <input type="button" class="btn_number btn_type" value="주민번호 확인" onclick="ssnCheck()">
                         </div>
@@ -199,7 +205,7 @@
                             <span class="int_mobile">
                                 <input type="tel" name="mobileNo" id="mobileNo" class="int" placeholder="전화번호 입력" maxlength="13" >
                             </span>
-                            <a href="#" class="btn_number btn_type" onclick="mobileConfirm()"><input type="button" id="btnSend"><span class = "click">인증번호 받기</span></a>
+                            <!-- <a href="#" class="btn_number btn_type" onclick="mobileConfirm()"><input type="button" id="btnSend"><span class = "click">인증번호 받기</span></a> -->
                         </div>
                         <div class="box mobile_confirm_area" style="display: none;">
                             <span class="mobile_confirm">
