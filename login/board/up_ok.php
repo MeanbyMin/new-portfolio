db08a6d6763a8fb0295bc97ce9edf84a2dc9be8c
MeanbyMin <soeg0810@gmail.com>
2021-02-10 21:49:39 +0900

    추천 php

---
+++ login/board/up_ok.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    include "../include/dbconn.php";
+    include "../include/sessionCheck.php";
+
+    $b_idx = $_GET['b_idx'];
+
+    if(!$conn){
+        echo "데이터베이스 연결 실패";
+    }else{
+        $sql = "UPDATE min_board SET b_up = b_up + 1 WHERE b_idx='$b_idx'";
+        $result = mysqli_query($conn, $sql);
+
+        $sql = "SELECT b_up FROM min_board WHERE b_idx='$b_idx'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($result);
+        echo $row['b_up'];
+    }
+?>
