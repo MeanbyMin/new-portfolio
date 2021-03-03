33f39b3e31b01e729e695bcb1459783452d6c573
MeanbyMin <soeg0810@gmail.com>
2021-03-03 15:13:27 +0900

    스크롤변화 수정

--- js/main.js
+++ js/main.js
@@ -11,11 +11,15 @@ let TextEnd = setTimeout(loadingtext, 2000);
 
 // 스크롤에 따른 변화
 window.addEventListener('scroll', () => {
-    let scrollLocation = document.documentElement.scrollTop; // 현재 스크롤바 위치
-    let windowHeight = window.innerHeight; // 스크린 창
-    let fullHeight = document.body.scrollHeight; //  margin 값은 포함 x
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
 
-    if(scrollLocation/100 >= 0 && scrollLocation/100 <= 0.8){
+    // contentHeight : 눈에 보이지 않는 남은 범위
+    const contentHeight = scrollHeight - clientHeight;
+    const percent = (scrollTop / contentHeight) * 100;
+
+    if(percent<1.5){
         document.querySelector(".main_text").style.opacity = "1";
         document.querySelector(".main_logo").style.opacity = "1";
         document.querySelector(".main_desc").style.opacity = "1";
