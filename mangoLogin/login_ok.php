8ad2a43a4d26b3e98251bc12078a2aebf4ec4ab4
MeanbyMin <soeg0810@gmail.com>
2021-03-17 18:33:07 +0900

    login_ok 페이지 수정

--- mangoLogin/login_ok.php
+++ mangoLogin/login_ok.php
@@ -4,24 +4,9 @@
     session_start();
     $userid = $_POST['userid'];
     $userpw = $_POST['userpw'];
-    $_SESSION['id'] = $userid;   
-?>
-<!DOCTYPE html>
-<html lang="kor">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>로그인</title>
-</head>
-<body>
-<?php
+
     if(($userid == "admin" && $userpw == "1234") || ($userid == "soeg0810" && $userpw == "12!@qw")){
-?>
-    <script>
-        alert("관리자님 어서오세요.");
-        location.href="./dist/index.php";
-    </script>
-<?php
+        echo "<script>alert('관리자님 어서오세요.'); location.href='./dist/index.php';</script>";
     }else{
 ?>
     <script>
@@ -31,5 +16,3 @@
 <?php
     }
 ?>
-</body>
-</html>
