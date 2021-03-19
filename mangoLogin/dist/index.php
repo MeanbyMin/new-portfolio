2e579886fb28a651a7879ecdcb88cd7e5fe0c412
MeanbyMin <soeg0810@gmail.com>
2021-03-19 21:27:10 +0900

    수정

--- mangoLogin/dist/index.php
+++ mangoLogin/dist/index.php
@@ -49,7 +49,7 @@
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="index.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
@@ -62,8 +62,8 @@
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="./RestaurantList.php">Restaurant List</a>
-                                    <a class="nav-link" href="layout-static.html">Mango Story</a>
-                                    <a class="nav-link" href="layout-static.html">Matjib List</a>
+                                    <a class="nav-link" href="MangoStory.php">Mango Story</a>
+                                    <a class="nav-link" href="MatjibList.php">Matjib List</a>
                                     <!-- <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a> -->
                                 </nav>
                             </div>
@@ -98,15 +98,6 @@
                                     </div>
                                 </nav>
                             </div> -->
-                            <div class="sb-sidenav-menu-heading">Addons</div>
-                            <a class="nav-link" href="charts.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Charts
-                            </a>
-                            <a class="nav-link" href="tables.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tables
-                            </a>
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
