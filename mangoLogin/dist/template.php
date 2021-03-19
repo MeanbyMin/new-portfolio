b41d24d2fd477f07c6e301525e67fc7d17422344
MeanbyMin <soeg0810@gmail.com>
2021-03-19 21:23:00 +0900

    template 수정

--- mangoLogin/dist/template.php
+++ mangoLogin/dist/template.php
@@ -49,7 +49,7 @@
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="index.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
@@ -61,9 +61,9 @@
                             </a>
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="RestaurantList.php">Restaurant Enrollment</a>
-                                    <a class="nav-link" href="layout-static.html">Mango Story</a>
-                                    <a class="nav-link" href="layout-static.html">Matjib List</a>
+                                    <a class="nav-link" href="./RestaurantList.php">Restaurant List</a>
+                                    <a class="nav-link" href="MangoStory.php">Mango Story</a>
+                                    <a class="nav-link" href="MatjibList.php">Matjib List</a>
                                 </nav>
                             </div>
                             <div class="sb-sidenav-menu-heading">Addons</div>
