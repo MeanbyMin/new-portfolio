60c048381c3a89d3396c2ae9545b67cb71c87d8c
MeanbyMin <soeg0810@gmail.com>
2021-05-25 20:47:26 +0900

    맛집리스트 태그 코드수정

--- mangoplate/tags.php
+++ mangoplate/tags.php
@@ -4,7 +4,7 @@
     include "./include/dbconn.php"; //php 파일 삽입
 
     $tag = $_GET['tag'];
-    $sql = "SELECT tl_idx, tl_title, tl_subtitle, tl_repphoto FROM top_lists WHERE tl_status = '등록' AND tl_title LIKE '%$tag%' LIMIT 20";
+    $sql = "SELECT tl_idx, tl_title, tl_subtitle, tl_repphoto FROM top_lists WHERE tl_status = '등록' AND tl_title LIKE '%$tag%' ORDER BY tl_idx DESC LIMIT 20";
     $result = mysqli_query($conn, $sql);
     while($row = mysqli_fetch_array($result)){
         echo $row['tl_idx']."&nbsp".$row['tl_title']."&nbsp".$row['tl_subtitle']."&nbsp".$row['tl_repphoto']."<br>";
