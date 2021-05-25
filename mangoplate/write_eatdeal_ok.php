cf5ea78c666019bae0a7c5ddf71036fbda5ec469
MeanbyMin <soeg0810@gmail.com>
2021-05-26 01:43:34 +0900

    관리자페이지 eat딜 작성페이지 수정

--- mangoplate/write_eatdeal_ok.php
+++ mangoplate/write_eatdeal_ok.php
@@ -51,24 +51,6 @@
             $rename = $ext[0].time();   // 같은 이름이 올라오면 덮어쓰기가 되기 때문에 이름을 재설정을 해줘야한다. 
             $rename = $rename.".".$ext[$length-1];
             $ext = strtolower(array_pop($ext));
-            // array_pop() : 스택구조. 마지막 값을 뽑이 내고 그 값을 반환합니다. 해당 데이터는 array에서 사라집니다.
-            
-
-            // if($error != UPLOAD_ERR_OK){    // UPLOAD_ERR_OK(0) -> 오류 없이 파일 업로드 성공
-            //     switch($error){
-            //         case UPLOAD_ERR_INI_SIZE:   // php.ini에 설정된 최대 파일을 초과했을 때
-            //         case UPLOAD_ERR_FORM_SIZE:  // HTML 폼에 설정된 최대 파일을 초과
-            //             echo "파일 크기가 너무 큽니다.";
-            //             break;
-            //         case UPLOAD_ERR_NO_FILE:    // 파일이 제대로 업로드 되지 않았을 경우
-            //             echo "파일이 제대로 첨부되지 않았습니다.";
-            //             break;
-            //         default:
-            //             echo $error;
-            //             echo "파일 업로드 실패";
-            //     }
-            //     exit;
-            // }
 
             if(!in_array($ext, $allowed_ext)){
                 echo "허용되지 않은 확장명입니다.";
@@ -87,10 +69,10 @@
         $sql = "INSERT INTO eat_deals (ed_userid, ed_region, ed_restaurant, ed_menu, ed_price, ed_percent, ed_startday, ed_endday, ed_resinfo, ed_menuinfo, ed_photo) VALUES ('$id', '$ed_region', '$ed_restaurant', '$ed_menu', '$ed_price', '$ed_percent', '$ed_startday', '$ed_endday',  '$ed_resinfostr', '$ed_menuinfostr', '$imgfile');";
         echo $sql;
         $result = mysqli_query($conn, $sql);
-        if(!$result){
-            echo "<script>alert('EatDeal 등록이 실패했습니다.'); history.back(); </script>";
-        }else{
-            echo "<script>alert('EatDeal 등록이 완료되었습니다.'); location.href='./EatDeal.php';</script>";
-        }
+        // if(!$result){
+        //     echo "<script>alert('EatDeal 등록이 실패했습니다.'); history.back(); </script>";
+        // }else{
+        //     echo "<script>alert('EatDeal 등록이 완료되었습니다.'); location.href='./EatDeal.php';</script>";
+        // }
     }
 ?>
